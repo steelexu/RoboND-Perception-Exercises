7af4c306175c2cd4614042c7ad4799a26289fedb
steele <steele_xu@126.com>
2017-10-22 17:25:01 +0800

    test2 pass by 500 data collection

--- Exercise-3/sensor_stick/scripts/capture_features.py
+++ Exercise-3/sensor_stick/scripts/capture_features.py
@@ -37,7 +37,7 @@ if __name__ == '__main__':
     for model_name in models:
         spawn_model(model_name)
 
-        for i in range(20):
+        for i in range(500):
             # make five attempts to get a valid a point cloud then give up
             sample_was_good = False
             try_count = 0
